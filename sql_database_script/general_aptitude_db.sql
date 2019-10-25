03312019bca84b913cadaaeb191c03cbada24d2b
Kristijonas <kristijonasgerdvilis@gmail.com>
2019-10-25 08:29:48 +0300

    Update general_aptitude_db.sql

--- sql_database_script/general_aptitude_db.sql
+++ sql_database_script/general_aptitude_db.sql
@@ -43,20 +43,20 @@ CREATE TABLE `messages` (
 INSERT INTO `messages` (`ID`, `UserName`, `SurName`, `Email`, `Message`, `Birthday`, `Date`) VALUES
 (119, 'Jonas', ' Jo', 'asbilas@gmail.com', 'asdsadsa', '1992-05-20', '2018-02-07 19:25:44'),
 (127, 'Naujas', ' Ä®raÅ¡as', 'info@gmail.com', 'adasdas', '1993-06-20', '2018-02-07 21:30:03'),
-(117, 'Kristijonas', ' Gerdvilis', 'info@gmail.com', 'sadsads', '1992-05-20', '2018-02-07 19:24:06'),
+(117, 'Something', ' Else', 'info@gmail.com', 'sadsads', '1992-05-20', '2018-02-07 19:24:06'),
 (89, 'As', ' Bilas', 'asbilas@gmail.com', 'Hahaha', '1990-08-10', '2018-02-05 17:35:13'),
-(92, 'Bill', ' Gates', 'bilgates@ha.com', 'Hello There!', '1960-02-22', '2018-02-05 17:38:01'),
-(129, 'Kristijonas', ' Gerdvilis', 'anicetap@gmail.com', 'asdasdsa', '1992-05-20', '2018-02-07 21:49:05'),
+(92, 'Bill', ' Test', 'bilgates@ha.com', 'Hello There!', '1960-02-22', '2018-02-05 17:38:01'),
+(129, 'Name', ' LastName', 'anicetap@gmail.com', 'asdasdsa', '1992-05-20', '2018-02-07 21:49:05'),
 (125, 'test', ' name', 'info@gmail.com', 'asdsad', '1992-05-20', '2018-02-07 21:24:41'),
-(126, 'Kristijonas', ' Gerdvilis', 'kristijonasgerdvilis@gmail.com', 'asdasd', '1992-05-20', '2018-02-07 21:29:35'),
+(126, 'John', ' Qwerty', 'asdsssa@gmail.com', 'asdasd', '1992-05-20', '2018-02-07 21:29:35'),
 (120, 'Albina', ' Juozapavicius', 'albinas@gmail.com', 'Sveiki atvyke', '1986-04-20', '2018-02-07 19:39:17'),
-(121, 'Kristijonas', ' Gerdvilis', 'kristijonasgerdvilis@gmail.com', 'sdfdsfsd', '1992-05-20', '2018-02-07 21:23:39'),
-(128, 'Kristijonas', ' Gerdvilis', 'kristijonasgerdvilis@gmail.com', 'asdsada', '1992-05-20', '2018-02-07 21:48:51'),
-(102, 'Kristijonas', ' Gerdvilis', 'kristijonasgerdvilis@gmail.com', 'asdsadasdasd', '1992-05-20', '2018-02-05 17:41:57'),
+(121, 'Povilas', ' Teterminas', 'asdsa@gmail.com', 'sdfdsfsd', '1992-05-20', '2018-02-07 21:23:39'),
+(128, 'Povilas', ' Teterminas', 'dsa@gmail.com', 'asdsada', '1992-05-20', '2018-02-07 21:48:51'),
+(102, 'Povilas', ' Teterminas', 'dasa@gmail.com', 'asdsadasdasd', '1992-05-20', '2018-02-05 17:41:57'),
 (130, 'test', ' name', 'asbilas@gmail.com', 'asdsadas', '1993-06-20', '2018-02-07 21:49:40'),
 (110, 'Josh', ' Abigeil', 'joshabigeil@gmail.com', 'Hello', '1995-06-18', '2018-02-05 20:31:04'),
 (106, 'test albinas', 'test albinas', 'info@gmail.com', 'asdsadsad', '1993-06-20', '2018-02-05 17:47:12'),
-(109, 'Kristijonas', ' Gerdvilis', 'kristijonasgerdvilis@gmail.com', 'fghfh', '1992-05-20', '2018-02-05 20:30:32'),
+(109, 'Qwerty', ' John', 'fsdadas@gmail.com', 'fghfh', '1992-05-20', '2018-02-05 20:30:32'),
 (111, 'Aniceta', ' Popa', 'anicetap@gmail.com', 'Sveikuciai', '1993-05-20', '2018-02-07 16:49:33');
 
 --
